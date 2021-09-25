2cfc2c50c1745dcfe52c3a38eb8587a424b256db
Kenda Shanti <kshanti@ev-e6.kzn.21-school.ru>
2021-09-25 18:23:22 +0300

    finish ex01 day07

--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -4,7 +4,11 @@
 int main(void){
 	int arrInt[]={1, 2, 3};
 	inter(arrInt, 3, &func);
+
 	double arrDouble[]={1.1, 2.2, 3.3};
 	inter(arrDouble, 3, &func);
+
+	std::string arrString[]={"Hello,", "World", "!"};
+	inter(arrString, 3, &func);
 	return (0);
 }
