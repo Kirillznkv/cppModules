a04e420daee4a2262b0f314bbe70de038980b4ba
kshanti_home <Zenich99@gmail.com>
2021-10-01 20:15:40 +0300

    finish day08

--- day08/ex02/main.cpp
+++ day08/ex02/main.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
+#include <list>
 #include "mutantstack.hpp"
 
-int main(int, char**)
+int main()
 {
-	return (0);
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
+
+// int main()
+// {
+// 	MutantStack<char> mutant;
+// 	std::list<char> list;
+// 	for (char i = 'a'; i <= 'z'; ++i){
+// 		mutant.push(i);
+// 		list.push_back(i);
+// 	}
+
+// 	MutantStack<char>::iterator mutantIterator;
+// 	std::list<char>::iterator listIterator;
+// 	mutantIterator = mutant.begin();
+// 	listIterator = list.begin();
+// 	std::cout<<"begin mutant:   |"<<*mutantIterator<<"|\t\t";
+// 	std::cout<<"begin list:   |"<<*listIterator<<"|"<<std::endl;
+// 	mutantIterator++;
+// 	listIterator++;
+// 	std::cout<<"begin+1 mutant: |"<<*mutantIterator<<"|\t\t";
+// 	std::cout<<"begin+1 list: |"<<*listIterator<<"|"<<std::endl;
+
+// 	MutantStack<char>::const_iterator mutantIteratorConst;
+// 	std::list<char>::const_iterator listIteratorConst;
+// 	mutantIteratorConst = mutant.cend();
+// 	listIteratorConst = list.cend();
+// 	std::cout<<"begin mutant:   |"<<*mutantIteratorConst<<"|\t\t";
+// 	std::cout<<"begin list:   |"<<*listIteratorConst<<"|"<<std::endl;
+// 	mutantIteratorConst--;
+// 	listIteratorConst--;
+// 	std::cout<<"begin-1 mutant: |"<<*mutantIteratorConst<<"|\t\t";
+// 	std::cout<<"begin-1 list: |"<<*listIteratorConst<<"|"<<std::endl;
+
+// 	return 0;
+// }
+
+//   метод cbegin() возвращает константный (только для чтения) итератор, представляющий начальный элемент контейнера;
+
+// https://ravesli.com/urok-198-iteratory-stl/
--- day08/ex02/mutantstack.hpp
+++ day08/ex02/mutantstack.hpp
@@ -4,8 +4,29 @@
 #include <stack>
 
 template<typename T>
-class mutantstack : std::stack<T>{
-
+class MutantStack : public std::stack<T>{
+private:
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	MutantStack(void) : std::stack<T>(){}
+	MutantStack(MutantStack const & copy) : std::stack<T>(copy){}
+	~MutantStack(void){};
+	MutantStack &operator=(MutantStack<T> const &op){
+		return (std::stack<T>::operator=(op));
+	}
+	iterator begin(void){
+		return (this->c.begin());
+	}
+	iterator end(void){
+		return (this->c.end());
+	}
+	const_iterator cbegin(void){
+		return (this->c.cbegin());
+	}
+	const_iterator cend(void){
+		return (this->c.cend());
+	}
 };
 
 #endif
